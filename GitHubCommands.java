5a85a2af1e7f6fd1ceebd48454c9cdb2a3ea7930
Henokk-14 <henokketsela14@gmail.com>
2021-04-06 20:11:09 -0400

    "added-some-of-the-functions"

--- GitHubCommands.java
+++ GitHubCommands.java
@@ -8,24 +8,26 @@ public class GitHubCommands {
     String repoPath = "/Users/henokk_14/Downloads/CSC 109/Git-Educated-Challenge-2";
     GitSubprocessClient gitSubprocessClient = new GitSubprocessClient(repoPath);
     String status = gitSubprocessClient.gitStatus();
-    //String gitAddFile = gitSubprocessClient.gitAddFile("GitHubCommands.java");
-    String gitAddAll = gitSubprocessClient.gitAddAll();
+   // GitAdd("GitHubCommands.java");
+    String gitAddFile = gitSubprocessClient.gitAddFile("GitHubCommands.java");
+    //String gitAddAll = gitSubprocessClient.gitAddAll();
     String commitMessage = "added-some-of-the-functions";
     String commit = gitSubprocessClient.gitCommit(commitMessage);
     String push = gitSubprocessClient.gitPush("main");
-    GitAdd("Helloa.java");
     System.out.println(status);
     //System.out.println(gitAddFile);
-    System.out.println(gitAddAll);
-    System.out.println(commit);
-    System.out.println(push);
+//    System.out.println(gitAddAll);
+//    System.out.println(commit);
+//    System.out.println(push);
   }
 
   public static void GitAdd(String filename) {
     try {
       String gitAddFile = gitSubprocessClient.gitAddFile(filename);
     } catch (Exception e) {
-      System.out.println("Error file name entered incorrect.");
+      e.printStackTrace();
+      System.out.println("Error file name entered incorrect file:"+ filename+ " Doesn't exist in scope" );
     }
   }
+
 }

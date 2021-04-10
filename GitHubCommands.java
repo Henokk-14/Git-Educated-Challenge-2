4ac1a844c99c4cb584e5518bd1437c932fa20987
Henokk-14 <henokketsela14@gmail.com>
2021-04-10 14:14:07 -0400

    "changed-GitAdd-to-a-functions"

--- GitHubCommands.java
+++ GitHubCommands.java
@@ -14,17 +14,18 @@ public class GitHubCommands {
     String commitMessage = "changed-GitAdd-to-a-functions";
     String commit = gitSubprocessClient.gitCommit(commitMessage);
     String push = gitSubprocessClient.gitPush("main");
-    System.out.println(status);
+    //System.out.println(status);
   }
   public static void main(String[] args){
      GitHubCommands run =  new GitHubCommands();
   }
 
-  public synchronized void GitAdd(GitSubprocessClient gitSubprocessClient,String filename) {
+  public void GitAdd(GitSubprocessClient gitSubprocessClient,String filename) {
     try {
       String gitAddFile = gitSubprocessClient.gitAddFile(filename);
       System.out.println("added "+ filename);
       String status = gitSubprocessClient.gitStatus();
+      System.out.println(status);
     } catch (Exception e) {
       e.printStackTrace();
       System.out.println("Error file name entered incorrect file:"+ filename+ " Doesn't exist in scope" );

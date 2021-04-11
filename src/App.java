dcda83a58a67829a9d7ad4fbfd5308a8d60f814f
Andrew231456 <andrewmatos203@gmail.com>
2021-04-11 18:49:10 -0400

    Finalize GUI Implementation + Action Events

--- src/App.java
+++ src/App.java
@@ -1,8 +1,17 @@
+/*
+ * Title: Git-Educated-Challenge-2
+ * GUI made by: Andrew Matos & Jack Handy
+ * Backend Programming by: 
+ * Date: 4/11/21
+ */
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JComponent;
@@ -12,8 +21,7 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JSplitPane;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
+
 
 
 public class App extends JPanel {
@@ -21,7 +29,10 @@ public class App extends JPanel {
 	Color lightMode = Color.white;
 	Color darkMode = Color.getHSBColor(convertRGBtoHSV(25,25,25)[0],convertRGBtoHSV(25,25,25)[1],convertRGBtoHSV(25,25,25)[2]);
 	Color greyMode = Color.getHSBColor(convertRGBtoHSV(40,40,40)[0],convertRGBtoHSV(40,40,40)[1],convertRGBtoHSV(40,40,40)[2]);
+	Color lightgreyMode = Color.getHSBColor(convertRGBtoHSV(180,180,180)[0],convertRGBtoHSV(180,180,180)[1],convertRGBtoHSV(180,180,180)[2]);
 	Color buttonColor = Color.getHSBColor(convertRGBtoHSV(60,60,60)[0],convertRGBtoHSV(60,60,60)[1],convertRGBtoHSV(60,60,60)[2]);
+	
+	
 	
 	public float[] convertRGBtoHSV(int R, int B, int G) {
 		float[] hsb = Color.RGBtoHSB(R,G,B,null);
@@ -97,6 +108,7 @@ public class App extends JPanel {
         JPanel westPanel = new JPanel();
         JPanel leftSide = new JPanel(new GridLayout());
         JPanel rightSide = new JPanel(new GridLayout());
+        
         // CREATING JTEXTAREAS
         JTextArea inputFrame = new JTextArea("Enter Input Here");
         inputFrame.setEditable(true);
@@ -122,9 +134,123 @@ public class App extends JPanel {
         centerPanel.setBackground(greyMode);
         eastPanel.setBackground(darkMode);
         westPanel.setBackground(darkMode);
+        DrawingPanel.setBackground(darkMode);
+        
+        // ADDING ACTION EVENT LISTENERS
+        darkModeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Dark Mode: " + darkModeButton.isSelected());
+                //TODO DO STATUS CODE HERE
+                if (darkModeButton.isSelected() == true) {
+                	// dark mode enabled
+                	statusButton.setBackground(buttonColor);
+                    statusButton.setForeground(Color.white);
+                    addButton.setBackground(buttonColor);
+                    addButton.setForeground(Color.white);
+                    commitButton.setBackground(buttonColor);
+                    commitButton.setForeground(Color.white);
+                    pushButton.setBackground(buttonColor);
+                    pushButton.setForeground(Color.white);
+                    clButton.setBackground(buttonColor);
+                    clButton.setForeground(Color.white);
+                    
+                	titlePanel.setBackground(darkMode);
+                    TitleText.setForeground(Color.white);
+                	
+                	northPanel.setBackground(darkMode);
+                	darkModeButton.setBackground(darkMode);
+                    darkModeButton.setForeground(Color.white);
+                    
+                	southPanel.setBackground(darkMode);
+                	
+                	inputFrame.setForeground(Color.white);
+                    inputFrame.setBackground(greyMode);
+                	outputFrame.setForeground(Color.white);
+                    outputFrame.setBackground(greyMode);
+                    
+                	centerPanel.setBackground(greyMode);
+                    eastPanel.setBackground(darkMode);
+                    westPanel.setBackground(darkMode);
+                    DrawingPanel.setBackground(darkMode);
+                } else {
+                	// dark mode disabled
+                	statusButton.setBackground(lightgreyMode);
+                    statusButton.setForeground(Color.black);
+                    addButton.setBackground(lightgreyMode);
+                    addButton.setForeground(Color.black);
+                    commitButton.setBackground(lightgreyMode);
+                    commitButton.setForeground(Color.black);
+                    pushButton.setBackground(lightgreyMode);
+                    pushButton.setForeground(Color.black);
+                    clButton.setBackground(lightgreyMode);
+                    clButton.setForeground(Color.black);
+                    
+                	titlePanel.setBackground(Color.white);
+                    TitleText.setForeground(Color.black);
+                	
+                	northPanel.setBackground(Color.white);
+                	darkModeButton.setBackground(Color.white);
+                    darkModeButton.setForeground(Color.black);
+                    
+                	southPanel.setBackground(Color.white);
+                	
+                	inputFrame.setForeground(Color.black);
+                    inputFrame.setBackground(lightgreyMode);
+                	outputFrame.setForeground(Color.black);
+                    outputFrame.setBackground(lightgreyMode);
+                    
+                	centerPanel.setBackground(greyMode);
+                    eastPanel.setBackground(Color.white);
+                    westPanel.setBackground(Color.white);
+                    DrawingPanel.setBackground(darkMode);
+                }
+            }
+        });
+        
+        statusButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Status");
+                //TODO DO STATUS CODE HERE
+            }
+        });
+        
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Add");
+                //TODO DO ADD CODE HERE
+            }
+        });
+        
+        commitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Commit");
+                //TODO DO COMMIT CODE HERE
+            }
+        });
+        
+        pushButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Push");
+                //TODO DO PUSH CODE HERE
+            }
+        });
+        
+        clButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Clear Input");
+                inputFrame.setText("");
+                
+            }
+        });
         
         // ADDING PANEL TO JFRAME
-        DrawingPanel.setBackground(darkMode);
+        
         add(titlePanel,BorderLayout.NORTH);
         DrawingPanel.add(northPanel,BorderLayout.NORTH);
         DrawingPanel.add(southPanel,BorderLayout.SOUTH);

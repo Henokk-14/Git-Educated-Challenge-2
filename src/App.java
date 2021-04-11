9d663feb69b8a32ee0469b5cbda1ceaf74d04079
JHandy24 <jmhandy@quinnipiac.edu>
2021-04-11 15:47:01 -0400

    Added App class to start GUI window

--- src/App.java
+++ src/App.java
@@ -1,9 +1,21 @@
+import javax.swing.JFrame;
 
-public class App {
+public class App extends JFrame {
 
+	public App() {
+		super("Working Together Pt.3");
+		this.setSize(800, 600);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		// create a jpanel and add it to the frame this.add(new DrawingPanel());
+		this.add(new DrawingPanel());
+		
+		this.setVisible(true);
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		new App();
+		
 	}
 
 }

0acb25bb661c51705967ed4667fab78dfe7c506c
yadel23 <57417452+yadel23@users.noreply.github.com>
2021-04-13 14:52:28 -0400

    testing

--- src/App.java
+++ src/App.java
@@ -1,63 +1,58 @@
+package src;
 /*
  * Title: Git-Educated-Challenge-2
  * GUI made by: Andrew Matos & Jack Handy
- * Backend Programming by: 
+ * Backend Programming by:
  * Date: 4/11/21
  */
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
+import git.tools.client.GitSubprocessClient;
+
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JRadioButton;
-import javax.swing.JSplitPane;
-import javax.swing.JTextArea;
-
-
-
 public class App extends JPanel {
-	
+    private static String repoPath;
+    private GitSubprocessClient Client;
+    private GitHubCommands command;
+
 	Color lightMode = Color.white;
 	Color darkMode = Color.getHSBColor(convertRGBtoHSV(25,25,25)[0],convertRGBtoHSV(25,25,25)[1],convertRGBtoHSV(25,25,25)[2]);
 	Color greyMode = Color.getHSBColor(convertRGBtoHSV(40,40,40)[0],convertRGBtoHSV(40,40,40)[1],convertRGBtoHSV(40,40,40)[2]);
 	Color lightgreyMode = Color.getHSBColor(convertRGBtoHSV(180,180,180)[0],convertRGBtoHSV(180,180,180)[1],convertRGBtoHSV(180,180,180)[2]);
 	Color buttonColor = Color.getHSBColor(convertRGBtoHSV(60,60,60)[0],convertRGBtoHSV(60,60,60)[1],convertRGBtoHSV(60,60,60)[2]);
-	
-	
-	
+
+
+
 	public float[] convertRGBtoHSV(int R, int B, int G) {
 		float[] hsb = Color.RGBtoHSB(R,G,B,null);
-        
+
         float hue = hsb[0];
-         
+
         float saturation = hsb[1];
-         
+
         float brightness = hsb[2];
-         
+
         System.out.println("RGB [" + R + "," + G + "," + B + "] converted to HSB [" + hue + "," + saturation + "," + brightness + "]" );
         return hsb;
 	}
 
 	public App() {
         super(new BorderLayout());
-		
+
+      //  Client = new GitSubprocessClient(repoPath);
+        command = new GitHubCommands();
+
         // CREATING JFRAME
         JPanel DrawingPanel = new JPanel();
         BorderLayout layout = new BorderLayout();
         DrawingPanel.setLayout(layout);
-        
+
         // CREATING TEXT LABEL
         JLabel TitleText = new JLabel("Git Educated Challenge 2");
-        
+
         // CREATING BUTTONS
         JRadioButton darkModeButton = new JRadioButton("Dark Mode");
         JButton statusButton = new JButton("Status");
@@ -65,7 +60,7 @@ public class App extends JPanel {
         JButton commitButton = new JButton("Commit");
         JButton pushButton = new JButton("Push");
         JButton clButton = new JButton("Clear Input");
-        
+
         // SETTING BUTTONS
         statusButton.setBackground(buttonColor);
         statusButton.setForeground(Color.CYAN);
@@ -77,13 +72,13 @@ public class App extends JPanel {
         pushButton.setForeground(Color.CYAN);
         clButton.setBackground(buttonColor);
         clButton.setForeground(Color.CYAN);
-        
+
         // CREATING TITLE TEXT
         JPanel titlePanel = new JPanel(new FlowLayout());
         titlePanel.setBackground(darkMode);
         titlePanel.add(TitleText);
         TitleText.setForeground(Color.white);
-        
+
         // DARK MODE BUTTON
         JPanel northPanel = new JPanel(new GridLayout());
         northPanel.setBackground(darkMode);
@@ -91,8 +86,8 @@ public class App extends JPanel {
         darkModeButton.setBackground(darkMode);
         darkModeButton.setForeground(Color.WHITE);
         darkModeButton.setSelected(true);
-        
-        
+
+
         // RADIO BUTTONS
         JPanel southPanel = new JPanel(new FlowLayout());
         southPanel.setBackground(darkMode);
@@ -101,41 +96,41 @@ public class App extends JPanel {
         southPanel.add(commitButton);
         southPanel.add(pushButton);
         southPanel.add(clButton);
-        
+
         // TEXT BOXES AND OUTPUT
         JPanel centerPanel = new JPanel(new GridLayout());
         JPanel eastPanel = new JPanel();
         JPanel westPanel = new JPanel();
         JPanel leftSide = new JPanel(new GridLayout());
         JPanel rightSide = new JPanel(new GridLayout());
-        
+
         // CREATING JTEXTAREAS
-        JTextArea inputFrame = new JTextArea("Enter Input Here");
+        JTextArea inputFrame = new JTextArea("");
         inputFrame.setEditable(true);
-        inputFrame.setLineWrap(true);  
-        inputFrame.setWrapStyleWord(true);  
+        inputFrame.setLineWrap(true);
+        inputFrame.setWrapStyleWord(true);
         inputFrame.setForeground(Color.white);
         inputFrame.setBackground(greyMode);
 
-        JTextArea outputFrame = new JTextArea("Output...");
+        JTextArea outputFrame = new JTextArea("Please enter the full repo directory on the left text box before checking status.");
         outputFrame.setEditable(false);
-        outputFrame.setLineWrap(true);  
-        outputFrame.setWrapStyleWord(true);  
+        outputFrame.setLineWrap(true);
+        outputFrame.setWrapStyleWord(true);
         outputFrame.setForeground(Color.white);
         outputFrame.setBackground(greyMode);
-        
+
         // CREATING JSPLITPANE
         JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftSide, rightSide);
         splitPane.setResizeWeight(0.52);
         leftSide.add(inputFrame);
         rightSide.add(outputFrame);
         centerPanel.add(splitPane);
-        
+
         centerPanel.setBackground(greyMode);
         eastPanel.setBackground(darkMode);
         westPanel.setBackground(darkMode);
         DrawingPanel.setBackground(darkMode);
-        
+
         // ADDING ACTION EVENT LISTENERS
         darkModeButton.addActionListener(new ActionListener() {
             @Override
@@ -154,21 +149,21 @@ public class App extends JPanel {
                     pushButton.setForeground(Color.CYAN);
                     clButton.setBackground(buttonColor);
                     clButton.setForeground(Color.CYAN);
-                    
+
                 	titlePanel.setBackground(darkMode);
                     TitleText.setForeground(Color.white);
-                	
+
                 	northPanel.setBackground(darkMode);
                 	darkModeButton.setBackground(darkMode);
                     darkModeButton.setForeground(Color.white);
-                    
+
                 	southPanel.setBackground(darkMode);
-                	
+
                 	inputFrame.setForeground(Color.white);
                     inputFrame.setBackground(greyMode);
                 	outputFrame.setForeground(Color.white);
                     outputFrame.setBackground(greyMode);
-                    
+
                 	centerPanel.setBackground(greyMode);
                     eastPanel.setBackground(darkMode);
                     westPanel.setBackground(darkMode);
@@ -185,21 +180,21 @@ public class App extends JPanel {
                     pushButton.setForeground(Color.black);
                     clButton.setBackground(lightgreyMode);
                     clButton.setForeground(Color.black);
-                    
+
                 	titlePanel.setBackground(Color.white);
                     TitleText.setForeground(Color.black);
-                	
+
                 	northPanel.setBackground(Color.white);
                 	darkModeButton.setBackground(Color.white);
                     darkModeButton.setForeground(Color.black);
-                    
+
                 	southPanel.setBackground(Color.white);
-                	
+
                 	inputFrame.setForeground(Color.black);
                     inputFrame.setBackground(lightgreyMode);
                 	outputFrame.setForeground(Color.black);
                     outputFrame.setBackground(lightgreyMode);
-                    
+
                 	centerPanel.setBackground(greyMode);
                     eastPanel.setBackground(Color.white);
                     westPanel.setBackground(Color.white);
@@ -207,31 +202,36 @@ public class App extends JPanel {
                 }
             }
         });
-        
+
         statusButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("Status");
-                //TODO DO STATUS CODE HERE
+                repoPath = inputFrame.getText();
+                Client = new GitSubprocessClient(repoPath);
+                String outputTxt = command.GitStatus(Client, repoPath);
+                outputFrame.setText(outputTxt);
             }
         });
-        
+
         addButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Add");
                 //TODO DO ADD CODE HERE
+                String fileName = inputFrame.getText();
+                outputFrame.setText(command.GitAdd(Client, fileName));
             }
         });
-        
+
         commitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Commit");
                 //TODO DO COMMIT CODE HERE
+
             }
         });
-        
+
         pushButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -239,52 +239,52 @@ public class App extends JPanel {
                 //TODO DO PUSH CODE HERE
             }
         });
-        
+
         clButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Clear Input");
                 inputFrame.setText("");
-                
+
             }
         });
-        
+
         // ADDING PANEL TO JFRAME
-        
+
         add(titlePanel,BorderLayout.NORTH);
         DrawingPanel.add(northPanel,BorderLayout.NORTH);
         DrawingPanel.add(southPanel,BorderLayout.SOUTH);
         DrawingPanel.add(centerPanel,BorderLayout.CENTER);
         DrawingPanel.add(eastPanel,BorderLayout.EAST);
         DrawingPanel.add(westPanel,BorderLayout.WEST);
-        
+
         add(DrawingPanel);
-        
+
     }
-	
+
 	private static void createAndShowGUI(Runnable t) {
         //Create and set up the window.
         JFrame frame = new JFrame("Working Together Pt.3");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
         JComponent newContentPane = new App();
         newContentPane.setOpaque(true);
         frame.setContentPane(newContentPane);
-        
-        
-        
+
+
+
         frame.getContentPane().setBackground(Color.BLACK);
         frame.setResizable(false);
         frame.pack();
-        
+
         frame.setVisible(true);
     }
-	
-	
-	@Override  
-    public Dimension getPreferredSize() {  
+
+
+	@Override
+    public Dimension getPreferredSize() {
         return new Dimension(800,500);  // CHANGES JFRAME DIMENSIONS
-    }  
+    }
 
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
@@ -295,3 +295,6 @@ public class App extends JPanel {
     }
 
 }
+
+
+

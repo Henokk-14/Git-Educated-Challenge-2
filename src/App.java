5eb114e4e6062c55f1c72d18200f4a4dcc35118c
Henokk-14 <henokketsela14@gmail.com>
2021-04-13 18:03:24 -0400

    "made-final-push-with-bug-and-feature-fixes"

--- src/App.java
+++ src/App.java
@@ -213,6 +213,8 @@ public class App extends JPanel {
                     repoPath = "";
                 }
                 outputFrame.setText(outputTxt);
+
+                
             }
         });
 
